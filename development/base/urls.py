e978314a2e1c1fb6c58b00178d33dfd96eea4960
bishaldan <124037829+bishaldan@users.noreply.github.com>
2024-02-18 11:45:14 +0545

    some changes of the datasheet

--- development/base/urls.py
+++ development/base/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('room/', views.room, name="room"),
-    path('login.html', views.login_page, name='login_page_html'),
+    path('login/', views.login_page, name='login_page'),
+    path('main_student/', views.main_student, name='main_student'),
+    path('chatbot_student/', views.chatbot_student, name='chatbot_student'),
 ]
